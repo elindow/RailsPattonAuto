a7c2180092b2016c20f3c99f675e57ad35bd2ffb
Lindow Eric <elindow@verizon.net>
2012-05-09 15:52:21 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPattonAuto::Application.config.secret_token = '81303b2b1c1e32b5c56f9342424233698edd0c81f45f697ea826caf05446400cb56f3a4c1e7d9d98ab536bf12df4d5608c6aed4df341bde1b24706191cf7b345'
